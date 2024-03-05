27a9abf17bc69303928d5d8f688db92d19458605
Mirsaid25 <siddikovmirsaid01@gmail.com>
2024-03-05 18:19:19 +0500

    anything

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({
     children: React.ReactNode;
     params: { lang: Locale };
 }>) {
-    <Script>AOS.init()</Script>;
+    // <Script>AOS.init()</Script>;
 
     const { footer, header, button } = await getDictionary(lang);
 
