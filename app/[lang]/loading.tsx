1c924b19cc81689a8edc74a32e329082edbec692
Mirsaid25 <siddikovmirsaid01@gmail.com>
2024-03-07 11:45:08 +0500

    add loading

--- app/[lang]/loading.tsx
+++ app/[lang]/loading.tsx
@@ -4,7 +4,6 @@ export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return (
         <div className="fixed top-0 left-0 bg-[#E7E7E7] h-screen w-full z-50 flex items-center justify-center animate-pulse">
-            <Image src="/images/footer-logo.png" width={500} height={200} alt="logo" className="w-[500px] invert-[100%] animate-pulse"/>
         </div>
     )
   }
